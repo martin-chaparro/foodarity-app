3620a9a6db35690afc4fc304099c37f54fd20224
Martin Chaparro <martin.a.chaparro@gmail.com>
2022-01-05 11:22:28 +0000

    feat: Add frontend Boilerplate

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,25 @@
+const RULES = {
+  OFF: 'off',
+  WARN: 'warn',
+  ERROR: 'error',
+};
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/react-in-jsx-scope': RULES.OFF,
+  },
+};
