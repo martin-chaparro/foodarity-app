d80130c547db2210109abe9e33fd29047dab9088
Javier Rostagno <88237623+jrostagno@users.noreply.github.com>
2022-01-07 09:45:45 -0300

    feat:link-landing

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -1,17 +1,17 @@
 /* eslint-disable consistent-return */
 /* eslint-disable import/prefer-default-export */
-import axios from 'axios'
+import axios from 'axios';
 
+export const REGISTER_LOCAL = 'REGISTER_LOCAL';
 
 export const registerLocal = (input) => async (dispatch) => {
-    try {
-        const res = await axios.post("/users", input)
-        return dispatch({
-            type: "REGISTER_LOCAL",
-            payload: res.data
-        })
-    }
-    catch (err) {
-        console.log(err);
-    }
-}
+  try {
+    const res = await axios.post('/users', input);
+    return dispatch({
+      type: REGISTER_LOCAL,
+      payload: res.data,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
