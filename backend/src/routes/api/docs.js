b13f6198159f51fac22e7dd2c581ad8357251d35
Martin Chaparro <martin.a.chaparro@gmail.com>
2022-01-10 17:59:02 +0000

    feat: add swagger documents

---
+++ backend/src/routes/api/docs.js
@@ -0,0 +1,18 @@
+const { Router } = require('express');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+
+
+const router = new Router();
+
+const swaggerDocument = YAML.load('./swagger.yaml');
+
+const options = {
+    customCss: '.swagger-ui .topbar { display: none }'
+  };
+
+
+router.use('/', swaggerUi.serve);
+router.get('/', swaggerUi.setup(swaggerDocument,options));
+
+module.exports = router;
