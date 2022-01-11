a27ac10498bf05c770a1b4c76b738d2ae45b2c47
Martin Chaparro <martin.a.chaparro@gmail.com>
2022-01-11 02:04:15 +0000

    fead(core): create base Boilerplate

---
+++ backoffice/src/Backoffice.js
@@ -0,0 +1,11 @@
+
+
+function Backoffice() {
+  return (
+    <div>
+      <h1>BackOffice</h1>
+    </div>
+  );
+}
+
+export default Backoffice;
---
+++ backoffice/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Backoffice from './Backoffice';
+
+ReactDOM.render(  
+    <Backoffice />,
+  document.getElementById('root')
+);
