363dc27e47cb861ab73d4adbd9c0e05b62dc7c9b
Manuel <manuelrmauro@gmail.com>
2022-01-11 17:10:28 -0300

    feat(product): redux & pagination

--- frontend/src/Components/Pagination/BasicPagination.jsx
+++ frontend/src/Components/Pagination/BasicPagination.jsx
@@ -1,22 +1,26 @@
 import * as React from 'react';
+import { useSelector } from 'react-redux';
 import styles from './Pagination.module.css';
 
-export default function Pagination({ paginado, products, productsPerPage }) {
+export default function Pagination({ paginado }) {
   const pageNumbers = [];
+  const page = useSelector((state) => state.product.page);
+  const pages = useSelector((state) => state.product.pages);
 
   // eslint-disable-next-line no-plusplus
-  for (let i = 1; i <= Math.ceil(products / productsPerPage); i++) {
+  for (let i = 1; i <= pages; i++) {
     pageNumbers.push(i);
-  }
+  } 
 
   return (
     <nav className={styles.botoneraContent}>
       <ul>
-        {pageNumbers &&
+        {pages > 1 &&
           pageNumbers.map((number) => {
             return (
               // eslint-disable-next-line react/button-has-type
               <button
+                disabled={page===number}
                 className={styles.btn}
                 key={number}
                 onClick={() => paginado(number)}
