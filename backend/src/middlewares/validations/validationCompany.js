f29e34c22b019385c0916c957bff3812c6282b72
KarenDerkach <86269842+KarenDerkach@users.noreply.github.com>
2022-01-13 14:14:59 -0300

    add- validation route update company

--- backend/src/middlewares/validations/validationCompany.js
+++ backend/src/middlewares/validations/validationCompany.js
@@ -13,6 +13,16 @@ const ValidationCompany = {
     check('cityId', 'ingrese ID de city').isNumeric(),
     check('stateId', 'Ingrese ID de state').isNumeric(),
   ],
+  update: [
+    check('name', 'Ingrese el nombre completo').not().isEmpty(),
+    check('email', 'Agrega un email válido').isEmail(),
+    check('description', 'Ingrese una descripcion').not().isEmpty(),
+    check('areaCode', 'Ingrese un codigo de area valido').isNumeric(),
+    check('phone', 'Ingrese un numero').isNumeric(), // TODO como validar bien numeros de telefono
+    check('street', 'Ingrese una direccion').not().isEmpty(),
+    check('number', 'Ingrese una altura').isNumeric(),
+    check('zipcode', 'Ingrese un codigo postal valido').isNumeric(),
+  ]
 };
 
 module.exports = ValidationCompany;
