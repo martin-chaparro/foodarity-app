39cf1e5a3dac2c63fdbb7c847193f36aa1a69d15
Javier Rostagno <88237623+jrostagno@users.noreply.github.com>
2022-01-14 17:28:21 -0300

    feat:profile-user-update

--- backend/src/controllers/companiesController.js
+++ backend/src/controllers/companiesController.js
@@ -38,8 +38,8 @@ const createCompany = async (req, res) => {
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-///////////////////////FOTO//////////////////////////////////////////////////
-const { tempFilePath } = req.files.file;
+
+    const { tempFilePath } = req.files.file;
 
     const { secure_url: secureUrl, public_id: publicId } =
       await cloudinary.uploader.upload(tempFilePath);
@@ -116,7 +116,7 @@ const searchCompany = async (req, res) => {
     const { id } = req.params;
     const company = await Company.findByPk(id, {
       include: [
-        { model: User, attributes:['id', 'name' , 'email'] },
+        { model: User, attributes: ['id', 'name', 'email'] },
         { model: CompanyType, as: 'type', attributes: ['type'] },
         {
           model: Address,
@@ -126,7 +126,6 @@ const searchCompany = async (req, res) => {
             { model: State, as: 'state' },
           ],
         },
-        
       ],
       attributes: {
         exclude: ['createdAt', 'updatedAt', 'CompanyTypeId'],
@@ -150,10 +149,10 @@ const searchCompanyByUser = async (req, res) => {
     const user = await User.findByPk(userId, {
       include: [{ model: Company, as: 'company' }],
     });
-    if (!user.CompanyId || user.CompanyId === null) {
+    if (!user.companyId || user.companyId === null) {
       return res.json({ msg: 'El usuario no posee una compañia' });
     }
-    return res.status(200).json(user.Company);
+    return res.status(200).json(user.company);
   } catch (error) {
     return res.status(500).send({ msg: error });
   }
@@ -369,12 +368,12 @@ const addUser = async (req, res) => {
 const deleteUser = async (req, res) => {
   try {
     let { id } = req.params;
-    id = parseInt(id,10)
+    id = parseInt(id, 10);
     const { userId } = req;
     const owner = await User.findByPk(userId, {
       include: { model: Company, as: 'company' },
     });
-    console.log('id params', id, 'user id', userId)
+    console.log('id params', id, 'user id', userId);
     if (!owner.companyId) {
       return res.status(400).json({
         message: 'El usuario no tiene compania',
@@ -383,7 +382,7 @@ const deleteUser = async (req, res) => {
     if (userId !== owner.company.ownerId) {
       if (userId !== id) {
         return res.status(401).json({ message: 'Not owner' });
-      } 
+      }
     }
     if (owner.company.status !== 'Habilitada') {
       return res.status(400).json({
@@ -411,15 +410,15 @@ const deleteUser = async (req, res) => {
     }
     user.setCompany(null);
     if (userId === id) {
-      return res.status(200).send({ message: `you exit the company '${owner.company.name}'` });
+      return res
+        .status(200)
+        .send({ message: `you exit the company '${owner.company.name}'` });
     }
     return res.status(200).send({ message: `${user.email} deleted` });
   } catch (error) {
     return res.status(500).send(error);
   }
 };
-
-
 
 module.exports = {
   getCompanies,
