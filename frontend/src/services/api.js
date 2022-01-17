74e944f981c9b278dcca200a85abddbc0a81d131
Martin Chaparro <martin.a.chaparro@gmail.com>
2022-01-17 08:59:38 +0000

    deploy backoffice_17_01

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,11 +3,11 @@ import axios from 'axios';
 const token = localStorage.getItem('token') || '';
 
 const api = axios.create({
-  baseURL: 'http://localhost:4000/api/v1',
+  baseURL: process.env.REACT_APP_BACKEND_URL,
 });
 
 const apiWithToken = axios.create({
-  baseURL: 'http://localhost:4000/api/v1',
+  baseURL: process.env.REACT_APP_BACKEND_URL,
   headers: { Authorization: `Bearer ${token}` },
 });
 
