953f7e132c3abfd21c6dad4c78599d91b984ec6a
Manuel <manuelrmauro@gmail.com>
2022-01-18 14:35:12 -0300

    feat/cart

---
+++ frontend/src/Pages/CompanyVisualizer/CompanyVisualizer.jsx
@@ -0,0 +1,87 @@
+import React, { useEffect, useState } from 'react';
+import {useSelector} from 'react-redux';
+import { useParams } from 'react-router-dom';
+// import Navbar from '../../Components/Navbar/NavbarCommerce';
+import CompanyProductCard from './CompanyProductCard';
+import styles from './CompanyVisualizer.module.css';
+import Banner from '../../assets/Banner.jpg';
+import { api, apiWithToken } from '../../services/api';
+
+export default function CompanyVisualizer() {
+  const [company, setcompany] = useState()
+  // eslint-disable-next-line no-unused-vars
+  const [products, setproducts] = useState()
+  const params = useParams()
+  const [user, setUser] = useState();
+  const { id } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+  api.get(`/companies/id/${params.id}`).then(res => setcompany(res.data))
+  api.get(`/products/company/${params.id}`).then(res => setproducts(res.data))
+  }, []);
+  
+  useEffect(() => {
+    if (id)
+    apiWithToken.get(`/users/${id}`).then((res) => {
+      setUser(res.data);
+    });
+  }, [id])
+
+  
+  // eslint-disable-next-line no-restricted-globals
+  window.scrollTo(0,0);
+  
+  
+  return (
+    <div className={styles.componentDiv}>
+      {/* <Navbar /> */}
+      <div className={styles.GeneralProfileImgs}>
+        <div className={styles.BannerDiv}>
+          <div className={styles.LogoDiv}>
+            <img className={styles.logoImg} src={company?.logo.url} alt="CompanyLogo" />
+        </div>
+          <img className={styles.bannerImg} src={Banner} alt="CompanyBanner" />
+          </div>
+      </div>
+      <div className={styles.infoContent}>
+     
+          <div className={styles.email}>
+          
+            <p>{company?.email}</p>
+          </div>
+          <div className={styles.phone}>
+            <p>Tel. Contacto: {company?.phone}</p>
+          </div>
+      
+          <div className={styles.stateCityNumber}>
+            <p className={styles.stateName}>{company?.address.state.name},  </p>
+            <p className={styles.cityName}>{company?.address.city.name}  </p>
+        
+          </div>
+          <div className={styles.street}>
+            <p className={styles.streetName}>Calle: {company?.address.street}</p>
+            <p className={styles.addressNumber}> {company?.address.number}  </p>
+          </div>
+          <div className={styles.zipcode}>
+            <p>Cód. Postal: {company?.address.zipcode}</p>
+          </div>
+      </div>
+      {company && company.company_type_id === 1 && 
+      <div className={styles.renderContainer}>
+      <div className={styles.divh2}>
+        <h2 className={styles.h2}>Publicaciones de la empresa</h2>
+      </div>
+         <div className={styles.divRenderCards}>
+         {products && products.map((product, index) => (
+                  // eslint-disable-next-line react/no-array-index-key
+                  <CompanyProductCard key={index} product={product} />
+         ))}
+        </div>
+      </div>}
+      {company && company.company_type_id === 2 && ((user && user.company && user.company.company_type_id === 1 && user.company.status === 'Habilitada') ?
+       <div>ACÁ VA EL COMPONENTE DEL FORMULARIO</div> : <div>ACA VA EL TEXTO QUE SE LE MUESTRA A UN USUARIO COMUN, SIN COMPANIA</div>)
+      
+      }
+    </div>
+  );
+}
---
+++ frontend/src/routers/AppRouter.js
@@ -0,0 +1,73 @@
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import Login from '../Pages/Loggin/Login';
+import Home from '../Components/HomePage/Home';
+import Landing from '../Pages/LandingPage/Landing';
+import Register from '../Pages/Register/RegisterFormUser';
+import Loading from '../Components/Loading/Loading';
+import ProfileCompany from '../Pages/ProfileCompany/ProfileCompany';
+import ProfileUser from '../Pages/ProfileUser/ProfileUser';
+// import AMyProfile from '../Pages/Profile User/AMyProfile';
+import CompanyVisualizer from '../Pages/CompanyVisualizer/CompanyVisualizer';
+import Navbar from '../Components/Navbar/Navbar';
+import { PrivateRoute } from './PrivateRoute';
+import { RollSelectorRouter } from './RollSelectorRouter';
+import { startCheking } from '../redux/actions/authActions';
+import MpTest from '../Components/MercadoPago/MpTest';
+import MpRedirect from '../Components/MercadoPago/MpRedirect';
+// import ErrorPage from '../Pages/Error/ErrorPage';
+
+export function AppRouter() {
+  const dispatch = useDispatch();
+  const { checking, id } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    dispatch(startCheking());
+  }, [dispatch]);
+
+  if (checking) {
+    return <Loading />;
+  }
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<Navbar />} />
+        {/* <Route path="*" element={<ErrorPage/>} />  */}
+      </Routes>
+      <Routes>
+        <Route exact path="/mptest" element={<MpTest />} />
+        <Route exact path="/mpredirect" element={<MpRedirect />} />
+        <Route
+          path="/profileuser"
+          element={
+            <PrivateRoute isAuisAuthenticated={!!id}>
+              <ProfileUser />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/profilecompany"
+          element={
+            <PrivateRoute isAuisAuthenticated={!!id}>
+              <ProfileCompany />
+            </PrivateRoute>
+          }
+        />
+        <Route path="/company/:id" element={<CompanyVisualizer />} />
+        <Route path="/home" element={<Home />} />
+        <Route
+          path="/rollselector/*"
+          element={
+            <PrivateRoute isAuisAuthenticated={!!id}>
+              <RollSelectorRouter />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+}
