0e9f0654e9ea6a5d0f46d91fa9804c47d93a5082
KarenDerkach <86269842+KarenDerkach@users.noreply.github.com>
2022-01-20 19:50:31 -0300

    feat/ connection mercado pago whit profile company

--- frontend/src/Components/MercadoPago/MpTest.jsx
+++ frontend/src/Components/MercadoPago/MpTest.jsx
@@ -1,6 +1,8 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react'
 import { apiWithToken } from '../../services/api'
+import styles from './MpTest.module.css'
+import logoMercadoPago from '../../assets/mercado_pago_png.png'
 
 function MpTest() {
   const handleOnClick = () => {
@@ -11,13 +13,11 @@ function MpTest() {
   }
   
   return (
-    <div>
-      <br/>
-      <br/>
-      <br/>
+    <div className={styles.container}>
      <h1>Registrar comercio</h1>
+      <img src={logoMercadoPago} alt='logo Mercado Pago' className={styles.img} />
      <p>Solo se tiene que mostrar si aun no activo Mercadopago</p>
-     <button type='button' onClick={handleOnClick} >Registrar comercio</button>
+     <button type='button' onClick={handleOnClick}  >Registrar comercio</button>
     </div>
   )
 }
