3855a28d76fa3d005c3acc5ad8ed29b8bcc79b21
Manuel <manuelrmauro@gmail.com>
2022-01-21 09:33:52 -0300

    feat/done-mp&cart

--- frontend/src/Components/MercadoPago/Success.jsx
+++ frontend/src/Components/MercadoPago/Success.jsx
@@ -1,25 +1,38 @@
 /* eslint-disable no-alert */
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import { apiWithToken } from '../../services/api';
 
 function Success() {
   const navigate = useNavigate();
   const [params] = useSearchParams();
-  // hay que traerse el ID por params
-  const orderId = 1;
+  const externalReference = params.get('external_reference');
+  const [redirect, setRedirect] = useState(false);
 
   useEffect(() => {
-    apiWithToken.put(`/orders/${orderId}`);
-  }, []);
+    apiWithToken.put(`/orders/${externalReference}`).then(() => {
+      setRedirect(true);
+    });
+  }, [externalReference]);
+
+  useEffect(() => {
+    if (redirect) {
+      alert('Compra realizada con exito');
+      navigate('/home');
+    }
+  }, [redirect]);
 
   return (
-    <div>
-      {
-        (alert('Compra realizada con exito'),
-        console.log(params),
-        navigate('/profilecompany'))
-      }
+    <div
+      style={{
+        display: 'flex',
+        width: '100vw',
+        height: '100vh',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      Redirecting...
     </div>
   );
 }
