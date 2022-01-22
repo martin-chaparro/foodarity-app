c3cd8e93dd68c0c45eba4f63405e5a43b8629a82
Javier Rostagno <88237623+jrostagno@users.noreply.github.com>
2022-01-22 17:57:34 -0300

    feat:data-copany

--- frontend/src/Components/ProfileCompanyComponents/Orders.jsx
+++ frontend/src/Components/ProfileCompanyComponents/Orders.jsx
@@ -23,18 +23,25 @@ const columns = [
 
   {
     id: 'total',
-    label: 'TOTAL',
+    label: 'PRECIO',
     minWidth: 170,
     align: 'right',
     format: (value) => value.toLocaleString('en-US'),
   },
   {
     id: 'precio',
-    label: 'PRECIO',
+    label: 'TOTAL',
     minWidth: 170,
     align: 'right',
     format: (value) => value.toLocaleString('en-US'),
   },
+  {
+    id: 'status',
+    label: 'ESTADO',
+    minWidth: 170,
+    align: 'right',
+  },
+
   {
     id: 'fecha',
     label: 'FECHA COMPRA',
@@ -44,44 +51,78 @@ const columns = [
   },
 ];
 
-/* const rows = [
-  createData('Lote pre-pizzas', 2, 120, '12/06/2022'),
-  createData('Lote facturas', '3', 400, '12/06/2022'),
-  createData('Lote tortas', '1', 100, '12/06/2022'),
-  createData('Lote panes', '4', 327, '12/06/2022'),
-  createData('Lote pan Rayado', '4', 376, '12/06/2022'),
-  createData('Lote combo pasteleria/rotiseria', '6', 2547, '12/06/2022'),
-]; */
-
 export default function Orders({ orders }) {
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
-  console.log(orders.orders);
+  console.log(orders);
+  console.log(orders[0].quantityByProduct[0].product);
 
   function createData(
     comprador,
     pago,
     lote,
     vendidos,
-
+    precio,
+    total,
     status,
+
     fecha
   ) {
     // const density = population / size;
-    return { comprador, pago, lote, vendidos, status, fecha };
+    return {
+      comprador,
+      pago,
+      lote,
+      vendidos,
+      precio,
+      total,
+      status,
+      fecha,
+    };
   }
 
-  const rows = orders.orders.map((order) =>
+  const rows = orders.map((order) =>
     createData(
       order.buyer.name,
       order.paymentMethod.method,
-      order.quantityByProduct[0].product_id,
+      order.quantityByProduct[0].product.lote,
       order.quantityByProduct[0].quantity,
+      order.quantityByProduct[0].product.price,
+      order.quantityByProduct[0].quantity *
+        order.quantityByProduct[0].product.price,
 
       order.status,
       order.date
     )
   );
+
+  // // eslint-disable-next-line prefer-const
+  // let rows = [];
+
+  // // eslint-disable-next-line no-plusplus
+  // for (let i = 0; i < orders.length; i++) {
+  //   while (orders[i].quantityByProduct.length) {
+  //     let j = orders[i].quantityByProduct.length;
+
+  //     rows.push(
+  //       createData(
+  //         orders[i].buyer.name,
+  //         orders[i].paymentMethod.method,
+  //         orders[i].quantityByProduct[j].product.lote,
+  //         orders[i].quantityByProduct[j].quantity,
+  //         orders[i].quantityByProduct[j].product.price,
+  //         orders[i].quantityByProduct[j].quantity *
+  //           orders[i].quantityByProduct[j].product.price,
+
+  //         orders[i].status,
+  //         orders[i].date
+  //       )
+  //     );
+
+  //     // eslint-disable-next-line no-plusplus
+  //     j--;
+  //   }
+  // }
 
   const handleChangePage = (newPage) => {
     setPage(newPage);
