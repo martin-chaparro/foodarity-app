bfb992f006a60f6ba4d813fba1e155a91faf420b
KarenDerkach <86269842+KarenDerkach@users.noreply.github.com>
2022-01-25 19:06:08 -0300

    fixes / styles

--- frontend/src/Pages/About/AboutMision.jsx
+++ frontend/src/Pages/About/AboutMision.jsx
@@ -10,10 +10,10 @@ export default function AboutMision() {
   return (
     <Card sx={{ width: 400 , height: 250, background: '#6C698D95',boxShadow: '4px 4px 10px rgba(0, 0, 0, 1)'}} className={styles.misionVision}>
       <CardContent>
-        <Typography sx={{ color: '#304543',fontWeight: 700, fontSize:'25px', borderRadius: 1, display: "flex", justifyContent: "center", alignItems: "center", textShadow: '1px 3px 0 #969696, 1px 13px 5px #aba8a8'}} variant="h5" component="div">
+        <Typography sx={{ color: '#FFFFFF',fontWeight: 700, fontSize:'25px', borderRadius: 1, display: "flex", justifyContent: "center", alignItems: "center", textShadow: '1px 3px 0 #969696, 1px 13px 5px #aba8a8'}} variant="h5" component="div">
         Mision
         </Typography>
-        <Typography sx={{ color: "#304543", marginTop: 2, fontSize:'15px', fontFamily: 'Tahoma'}} variant="body2">
+        <Typography sx={{ color: "#FFFFFF", marginTop: 2, fontSize:'15px', fontFamily: 'Tahoma'}} variant="body2">
         Evitar la pérdida de alimentos mientras contribuimos para reducir la
         producción de desperdicios y contaminación. Ayudamos a los comerciantes
         a minimizar perdidas mientras que damos oportunidad para que personas
