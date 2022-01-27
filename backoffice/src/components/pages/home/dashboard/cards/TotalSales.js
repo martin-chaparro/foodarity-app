1c6679d3abac8660565457dd99fe7a388d314007
Martin Chaparro <martin.a.chaparro@gmail.com>
2022-01-27 11:54:31 +0000

    deploy: fix graph

--- backoffice/src/components/pages/home/dashboard/cards/TotalSales.js
+++ backoffice/src/components/pages/home/dashboard/cards/TotalSales.js
@@ -1,21 +1,20 @@
 // import { Icon } from '@iconify/react';
 // import appleFilled from '@iconify/icons-ant-design/apple-filled';
-import {FaDollarSign} from 'react-icons/fa'
+import { FaDollarSign } from 'react-icons/fa';
 // material
 import { alpha, styled } from '@mui/material/styles';
 import { Card, Typography } from '@mui/material';
 // utils
 import { fShortenNumber } from '../../../../../helpers/formatNumber';
 
-
 const RootStyle = styled(Card)(({ theme }) => ({
   textAlign: 'center',
   padding: theme.spacing(5, 0),
-  borderRadius:'2em',
-  backgroundImage: `linear-gradient(135deg, ${alpha(theme.palette.error.main, 0)} 0%, ${alpha(
+  borderRadius: '2em',
+  backgroundImage: `linear-gradient(135deg, ${alpha(
     theme.palette.error.main,
-    0.24
-  )} 100%)`
+    0
+  )} 0%, ${alpha(theme.palette.error.main, 0.24)} 100%)`,
 }));
 
 const IconWrapperStyle = styled('div')(({ theme }) => ({
@@ -28,15 +27,13 @@ const IconWrapperStyle = styled('div')(({ theme }) => ({
   justifyContent: 'center',
   marginBottom: theme.spacing(3),
   color: theme.palette.error.main,
-  backgroundImage: `linear-gradient(135deg, ${alpha(theme.palette.error.main, 0)} 0%, ${alpha(
+  backgroundImage: `linear-gradient(135deg, ${alpha(
     theme.palette.error.main,
-    0.24
-  )} 100%)`
+    0
+  )} 0%, ${alpha(theme.palette.error.main, 0.24)} 100%)`,
 }));
 
-
-
-export default function TotalSales({quantity}) {
+export default function TotalSales({ quantity }) {
   return (
     <RootStyle>
       <IconWrapperStyle>
